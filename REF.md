000889e591e7e08174eb24d79f061b43b446f154
Jack.Win <foxintango@yeah.net>
2023-10-10 20:53:32 +0800

    alpine

--- REF.md
+++ REF.md
@@ -11,3 +11,8 @@ https://www.armbbs.cn/forum.php?mod=viewthread&tid=109952
 	https://gitee.com/wzh1845462801/dynamic_loader
 
 https://gitee.com/qq827992983/PE_and_ELF
+
+https://forums.freertos.org/t/dynamic-loader/12865/6
+	https://mcuoneclipse.com/2021/06/05/position-independent-code-with-gcc-for-arm-cortex-m/
+
+https://ourembeddeds.github.io/blog/2020/08/16/elf-loader/
---
+++ src/ELFFile.cpp
@@ -0,0 +1,14 @@
+#include "ELFFile.h"
+using namespace foxintango;
+
+#if defined(STM32F101x6) || defined(STM32F101xB) || defined(STM32F101xE) || defined(STM32F101xG) || \
+    defined(STM32F102x6) || defined(STM32F102xB) || \
+    defined(STM32F103x6) || defined(STM32F103xB) || defined(STM32F103xE) || defined(STM32F103xG) || \
+    defined(STM32F105xC) || defined(STM32F107xC)
+#define APOLLO_IN_KERNEL
+#endif
+
+#define APOLLO_IN_KERNEL
+
+#if defined(APOLLO_IN_KERNEL)
+#endif
