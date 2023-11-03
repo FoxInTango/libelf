69d9d92be619f46e6d6ec3f12f5054cdfa6ac120
Jack.Win <foxintango@yeah.net>
2023-11-03 16:53:20 +0800

    alpine

--- elf/liba/share.cpp
+++ elf/liba/share.cpp
@@ -13,4 +13,4 @@ int sub(){
  arm-none-eabi-g++ -c -fPIC -Wall -fvisibility=hidden -std=c++11 -o liba/share.o liba/share.cpp
  arm-none-eabi-g++ -fPIC -shared -lstdc++ -o out/liba.so lib/liba.o -Wl,-Map=./out/liba.map
  arm-none-eabi-objdump -h -S ./out/liba.so  > ./out/liba.lst
-/*
+*/
