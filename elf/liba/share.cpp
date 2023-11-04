53fdf0f646ab2dbeeafb4e61ca07b9f5d6c04480
Jack.Win <foxintango@yeah.net>
2023-11-04 20:38:41 +0800

    alpine

--- elf/liba/share.cpp
+++ elf/liba/share.cpp
@@ -1,14 +1,11 @@
 int a = 32;
-int b;
-int c;
-int d = 96;
-
-void empty(){}
 
 int add(){
     a = 0;
     return a;
 }
+
+/*
 
 int sub(){
     a = b * a;
@@ -19,6 +16,7 @@ int div() {
     a = 2;
     return 0;
 }
+*/
 /*
  arm-none-eabi-g++ -c -fPIC -O0 -g3 -Wall -fvisibility=hidden -std=c++11 -o liba/share.o liba/share.cpp
  arm-none-eabi-g++ -fPIC -shared -lstdc++ -o out/liba.so  liba/share.o -Wl,-Map=./out/liba.map
