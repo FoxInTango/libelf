6de754c041a5e22e07a3ffb9b22bd1e28819492e
Jack.Win <foxintango@yeah.net>
2023-11-09 17:21:19 +0800

    alpine

--- src/ELFFile.cpp
+++ src/ELFFile.cpp
@@ -52,23 +52,94 @@ size_t str_split(char* buffer,const size_t& length,char** strs,const size_t& str
     return str_count;
 }
 int ELFFile::open(const char* path){
-    FILE* file = fopen(path,"r");
-    char elf_magic[4];
-    char elf_bitwide;
-    char elf_edian;
-    char elf_version;
-    Endian edian;
-    // 参考 https://www.cnblogs.com/jiqingwu/p/elf_explore_2.html
-    if(file){
-        fread(&elf_magic,1,4,file);//
-        fread(&elf_bitwide,1,1,file);//5
-        fread(&elf_edian, 1, 1, file);//6
-        fread(&elf_version,1,1,file);
+    Endian endian = CurrentEndian();
 
-        printf("elf_magic:   %c%c%c%c\n", elf_magic[0], elf_magic[1], elf_magic[2], elf_magic[3]);
-        printf("elf_bitwide: %d\n", (int)elf_bitwide);
-        printf("elf_edian:   %d\n", (int)elf_edian);
-        printf("elf_version: %d\n", (int)elf_version);
+    FILE* file = fopen(path,"r");
+    // 参考 https://www.cnblogs.com/jiqingwu/p/elf_explore_2.html
+
+    /* e_type
+     
+     ET_NONE, 0, No file type
+     ET_REL, 1, Relocatable file（可重定位文件，通常是文件名以.o结尾，目标文件）
+     ET_EXEC, 2, Executable file （可执行文件）
+     ET_DYN, 3, Shared object file （动态库文件，你用gcc编译出的二进制往往也属于这种类型，惊讶吗？）
+     ET_CORE, 4, Core file （core文件，是core dump生成的吧？）
+     ET_NUM, 5，表示已经定义了5种文件类型
+     ET_LOPROC, 0xff00, Processor-specific
+     ET_HIPROC, 0xffff, Processor-specific
+     */
+
+    /* e_machine
+     EM_NONE, 0, No machine
+     EM_M32, 1, AT&T WE 32100
+     EM_SPARC, 2, SPARC
+     EM_386, 3, Intel 80386
+     EM_68K, 4, Motorola 68000
+     EM_88K, 5, Motorola 88000
+     EM_860, 7, Intel 80860
+     EM_MIPS, 8, MIPS RS3000
+     */
+    char e_i_magic[4];
+    char e_i_bitwide;
+    char e_i_endian;
+    char e_i_version;
+    uint16_t e_type;        //  2字节，描述了ELF文件的类型。
+    uint16_t e_machine;     //  2字节,描述了文件面向的架构，可取值如下（因为文档较老，现在有更多取值，参见/usr/include/elf.h中的EM_开头的宏定义）：
+    uint32_t e_version;     //  2字节,描述了ELF文件的版本号，合法取值如下：
+    uint32_t e_entry;       // （32位4字节，64位8字节），执行入口点，如果文件没有入口点，这个域保持0。
+    uint64_t e_entry_64;
+    uint32_t e_phoff;       //  (32位4字节，64位8字节），program header table的offset，如果文件没有PH，这个值是0。
+    uint64_t e_phoff_64;
+    uint32_t e_shoff;       //  (32位4字节，64位8字节）， section header table 的offset，如果文件没有SH，这个值是0。
+    uint64_t e_shoff_64;
+    uint32_t e_flags;       //  4字节，特定于处理器的标志，32位和64位Intel架构都没有定义标志，因此eflags的值是0。
+    uint16_t e_ehsize;      //  2字节，ELF header的大小，32位ELF是52字节，64位是64字节。
+    uint16_t e_phentsize;   //  2字节。program header table中每个入口的大小。
+    uint16_t e_phnum;       //  2字节。如果文件没有program header table, e_phnum的值为0。e_phentsize乘以e_phnum就得到了整个program header table的大小。
+    uint16_t e_shentsize;   //  2字节，section header table中entry的大小，即每个section header占多少字节。
+    uint16_t e_shnum;       //  2字节，section header table中header的数目。如果文件没有section header table, e_shnum的值为0。e_shentsize乘以e_shnum，就得到了整个section header table的大小。
+    uint16_t e_shstrndx     //  2字节。section header string table index.包含了section header table中section name string table。如果没有section name string table, e_shstrndx的值是SHN_UNDEF
+    
+    if(file){
+        fread(&e_magic,  1, 4, file);//
+        fread(&e_bitwide,1, 1, file);//5
+        fread(&e_edian,  1, 1, file);//6
+        fread(&e_version,1, 1, file);
+
+        fread(&e_type,    1, 2, file);
+        fread(&e_machine, 1, 2, file);
+        fread(&e_version, 1, 4, file);
+        if(e_i_bitwide == 1){
+            fread(&e_entry, 1, 4, file);
+            fread(&e_phoff, 1, 4, file);
+            fread(&e_shoff, 1, 4, file);
+        }else { 
+            fread(&e_entry_64, 1, 8, file);
+            fread(&e_phoff_64, 1, 8, file);
+            fread(&e_shoff_64, 1, 8, file);
+        }
+        fread(&e_flags,  1, 4, file);
+        fread(&e_ehsize, 1, 2, file);
+        fread(&e_phentsize, 1, 2, file);
+        fread(&e_phnum, 1, 2, file);
+        fread(&e_shentsize, 1, 2, file);
+        fread(&e_shnum,  1, 2, file);
+        fread(&e_phentsize, 1, 2, file);
+
+        // 处理字节序
+
+        if(endian != e_i_endian){
+            e_type    = endianSwap16u(e_type);
+            e_machine = endianSwap16u(e_machine);
+            e_version = endianSwap32u(e_version);
+        }
+        printf("elf_magic:   %c%c%c%c\n", e_i_magic[0], e_i_magic[1], e_i_magic[2], e_i_magic[3]);
+
+        printf("elf_bitwide: %d\n", (int)e_i_bitwide);
+        printf("elf_edian:   %d\n", (int)e_i_edian);
+        printf("elf_version: %d\n", (int)e_i_version);
+
+        
 
         size_t symtab_off = 0x000a0c;
         size_t strtab_off = 0x000d0c;
