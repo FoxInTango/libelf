4b86b20bd988dd04e50a9e9b8034c48871a0ce58
Jack.Win <foxintango@yeah.net>
2023-11-09 20:29:57 +0800

    alpine

--- src/ELFFile.cpp
+++ src/ELFFile.cpp
@@ -201,15 +201,17 @@ int ELFFile::open(const char* path){
 
         char** shstrs = new char* [128];
         size_t shstr_count = str_split(shstrtab, shstrtab_size, shstrs, 128);
-        /*
+        
         for(int i = 0;i < str_count;i ++){
             printf("str %d : %s str_size: %u\n",i,strs[i],string_length<char>(strs[i]));
         }
-        /*
+        
         for (int i = 0; i < shstr_count; i++) {
             printf("shstr %d : %s str_size: %u\n", i, shstrs[i], string_length<char>(shstrs[i]));
         }
-        */
+        //*/
+
+        /*
         for(int i = 0;i < strtab_size;i ++){
             if(strtab[i] == 0){
                 printf("\n");
@@ -226,7 +228,7 @@ int ELFFile::open(const char* path){
                 printf("%c", shstrtab[i]);
             }
         }
-        
+        */
         fclose(file);
         return 1;
     }
